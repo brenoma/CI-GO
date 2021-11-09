792e4d1a394c332fa95a45e65579d4b91a4a5d16
Breno Menezes <brenoma@live.com>
2021-11-09 20:21:44 -0300

    fix: changed tests to publish commits

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func testSum(t *testing.T) {
-	total := Sum(15, 15)
+	total := sum(15, 15)
 
 	if total != 30 {
 		t.Errorf("Resultado da soma é inválida, Resultado: %d - Esperado: %d", total, 30)
